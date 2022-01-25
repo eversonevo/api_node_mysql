78797e627fd9378067e3e50ac4642ff9bf135e19
Everson Evo <evotreinamentos@gmail.com>
2022-01-24 21:22:15 -0300

    bd_atualizado

--- mysql.js
+++ mysql.js
@@ -2,13 +2,10 @@ const mysql = require('mysql2');
 
 // variaveis de ambiente
 var pool = mysql.createPool({
-    "user": process.env.MYSQL_USER,
-    "password": process.env.MYSQK_PASSWORD,
-    "database": process.env.MYSQL_DATABASE,
-    "host": process.env.MYSQL_HOST,
-    "port": process.env.MYSQL_PORT,
+    "user": process.env.MYSQL_USER || "root",
+    "password": process.env.MYSQL_PASSWORD || "root",
+    "database": process.env.MYSQL_DATABASE || "ecommerce",
+    "host": process.env.MYSQL_HOST || "localhost",
+    "port": process.env.MYSQL_PORT || 3306,
 });
-
-console.log("eee"+pool.host);
-
 exports.pool = pool;
